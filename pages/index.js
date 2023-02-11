7c248fea541a0e3df5605d62bce688206d55bb2a
meganmelau <lau.m.megan@gmail.com>
2023-02-10 23:52:46 -0500

    FriendsPage Init

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import FriendsPage from './components/FriendsPage'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,9 +16,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <FriendsPage/>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Megan Was Here Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
